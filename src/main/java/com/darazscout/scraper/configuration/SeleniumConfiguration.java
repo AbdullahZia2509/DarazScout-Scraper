a465f5aeeb543c9300b0d1745f1949ad5b61f3c3
Abdullah Khan <Abdullah-khan@live.co.uk>
2021-01-02 23:26:24 +0500

    added more chrome options

--- src/main/java/com/darazscout/scraper/configuration/SeleniumConfiguration.java
+++ src/main/java/com/darazscout/scraper/configuration/SeleniumConfiguration.java
@@ -19,7 +19,7 @@ public class SeleniumConfiguration {
     public ChromeDriver driver() {
         final ChromeOptions options = new ChromeOptions();
         options.setBinary(".apt/usr/bin/google-chrome");
-        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors", "--enable-features=NetworkService,NetworkServiceInProcess", "--disable-dev-shm-usage");
         return new ChromeDriver(options);
     }
 
